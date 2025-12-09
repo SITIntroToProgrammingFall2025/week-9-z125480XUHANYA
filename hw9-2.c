d86600fadd8f37387c2a7303b3c5a98cabbd40b5
z125480XUHANYA <z125480@shibaura-it.ac.jp>
2025-12-10 02:24:36 +0900

    Add files via upload

--- hw9-2.c
+++ hw9-2.c
@@ -4,20 +4,20 @@ int main(void) {
     int a[3][3];
     int i, j;
 
-    /* input 3x3 array */
+    /* 读取 3x3 数组（按行读，共 9 个整数） */
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
             scanf("%d", &a[i][j]);
         }
     }
 
-    /* print original matrix */
+    /* 输出原矩阵 */
     printf("You entered\n");
     for (i = 0; i < 3; i++) {
         printf("%d %d %d\n", a[i][0], a[i][1], a[i][2]);
     }
 
-    /* print flipped result */
+    /* 输出水平翻转后的矩阵 */
     printf("Output\n");
     for (i = 0; i < 3; i++) {
         printf("%d %d %d\n", a[i][2], a[i][1], a[i][0]);
